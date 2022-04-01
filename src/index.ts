bf31ef7e12949a1c0b0a18cabcd0c59e4439f9df
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2022-04-02 00:56:30 +0400

    parse knowledge base to get quads

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Test");
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { Parser } from 'n3';
+
+const kbFilePath = resolve(__dirname, '../assets/kb.n3');
+const kb = readFileSync(kbFilePath, { encoding: 'utf8' });
+const parser = new Parser({ format: 'text/n3' });
+const quads = parser.parse(kb);
+
+console.log(`knowledge base contains ${quads.length} quads`);
